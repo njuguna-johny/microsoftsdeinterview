2c53474569280281345c8da9a9e7a457696d06e6
njuguna-johny <njuguna-johny@users.noreply.github.com>
2019-08-21 07:58:20 +0300

    Add files via upload

---
+++ EmployeeLib.Tests/EmployeesTest.cs
@@ -0,0 +1,108 @@
+﻿using EmployeeLib;
+using EmployeeLib.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+
+namespace Employee.UnitTests
+{
+    [TestClass]
+    public class EmployeesTest
+    {
+
+        [TestMethod]
+        public void TestForSalaryValidIntegers()
+        {
+            //Passing Test
+            var stringBuilder = new StringBuilder()
+                .AppendLine("Employee1,,1000")
+                 .AppendLine("Employee2,Employee1,800")
+               .AppendLine("Employee4,Employee2,500")
+               //Uncomment the line below to fail the text
+               // .AppendLine("Employee4,Employee2,5KLI0")
+                .AppendLine("Employee6,Employee2,500")
+               .AppendLine("Employee3,Employee1,500")
+                .AppendLine("Employee5,Employee1,500");
+            var employeeData = new Employees(stringBuilder.ToString());
+            Assert.IsTrue(employeeData.CheckIfSalariesAreValid);
+        }
+
+        [TestMethod]
+        public void TestForEmployeesWithMoreThanOneManager()
+        {
+            var stringBuilder = new StringBuilder()
+                .AppendLine("Employee1,,1000")
+                 .AppendLine("Employee2,Employee1,800")
+               .AppendLine("Employee4,Employee2,500")
+                //Uncomment the line below to fail the text
+                //.AppendLine("Employee4,Employee2,500")
+                .AppendLine("Employee6,Employee2,500")
+               .AppendLine("Employee3,Employee1,500")
+                .AppendLine("Employee5,Employee1,500");
+            var employeeData = new Employees(stringBuilder.ToString());
+            Assert.IsTrue(employeeData.ValidateEmployeesWithMoreThanOneManager);
+        }
+        [TestMethod]
+        public void TestForValidatingOneCEO()
+        {
+            var stringBuilder = new StringBuilder()
+                .AppendLine("Employee1,,1000")
+                 .AppendLine("Employee2,Employee1,800")
+               .AppendLine("Employee4,Employee2,500")
+               //Uncomment the line below to fail the text
+              // .AppendLine("Employee1,,1000")
+                .AppendLine("Employee6,Employee2,500")
+               .AppendLine("Employee3,Employee1,500")
+                .AppendLine("Employee5,Employee1,500");
+            var employeeData = new Employees(stringBuilder.ToString());
+            Assert.IsTrue(employeeData.ValidateOneCEO);
+        }
+
+        [TestMethod]
+        public void ValidateNonEmployeeManagers()
+        {
+            var stringBuilder = new StringBuilder()
+                .AppendLine("Employee1,,1000")
+                 //Uncomment the line below to fail the text
+                 //.AppendLine(",,1000")
+                 .AppendLine("Employee2,Employee1,800")
+               .AppendLine("Employee4,Employee2,500")
+                .AppendLine("Employee6,Employee2,500")
+               .AppendLine("Employee3,Employee1,500")
+                .AppendLine("Employee5,Employee1,500");
+            var employeeData = new Employees(stringBuilder.ToString());
+            Assert.IsTrue(employeeData.validateNonEmployeeManagers);
+        }
+
+        [TestMethod]
+        public void GetSalaryBudget()
+        {
+            var stringBuilder = new StringBuilder()
+                .AppendLine("Employee1,,1000")
+                 .AppendLine("Employee2,Employee1,800")
+               .AppendLine("Employee4,Employee2,500")
+                .AppendLine("Employee6,Employee2,500")
+               .AppendLine("Employee3,Employee1,500")
+                .AppendLine("Employee5,Employee1,500");
+            var employeeData = new Employees(stringBuilder.ToString());
+            Assert.AreEqual(2800,employeeData.ManagerSalaryBudget("Employee1"));
+        }
+
+        [TestMethod]
+        public void ValidateCircularRefence()
+        {
+            var stringBuilder = new StringBuilder()
+                .AppendLine("Employee1,,1000")
+                 .AppendLine("Employee2,Employee1,800")
+               //Uncomment the line below to fail the text
+               .AppendLine("Employee1,Employee2,800")
+               .AppendLine("Employee4,Employee2,500")
+                .AppendLine("Employee6,Employee2,500")
+               .AppendLine("Employee3,Employee1,500")
+                .AppendLine("Employee5,Employee1,500");
+            var employeeData = new Employees(stringBuilder.ToString());
+            Assert.IsTrue(employeeData.validateCircularReference);
+        }
+    }  
+    
+}
---
+++ EmployeeLib/Employees.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
+using EmployeeLib.Model;
+namespace EmployeeLib
+{
+    public class Employees 
+    {
+        private readonly string csvInput;
+        public List<Employee> _employeeList;
+        public bool CheckIfSalariesAreValid;
+        public bool ValidateEmployeesWithMoreThanOneManager;
+        public bool ValidateOneCEO;
+        public bool validateNonEmployeeManagers;
+        public bool validateCircularReference;
+        //Employee Constructor accepts string csc 
+        public Employees(string csv)
+        {
+            csvInput = csv;
+            _employeeList = new List<Employee>();
+            //Load from CSV to List
+            LoadEmployees();
+            //Check for valid salaries
+            CheckIfSalariesAreValid = ValidateSalaries(_employeeList);
+            //Checl for employees with more than one manager
+            ValidateEmployeesWithMoreThanOneManager = ValidateEmployees(_employeeList);
+            //Validate CEO
+            ValidateOneCEO = ValidateCEO(_employeeList);
+            //Validate non employee managers
+            validateNonEmployeeManagers = ValidateNonEmployeeManagers(_employeeList);
+            //Check circular reference
+            validateCircularReference = ValidateCircularReference(_employeeList);
+        }
+        
+        //Sets employees list
+        private void LoadEmployees()
+        {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(false, ',');
+            CsvReaderOptions csvReaderOptions = new CsvReaderOptions(new[] { Environment.NewLine });
+            var csvMapper = new MappingHelper();
+            CsvParser<Employee> csvParser = new CsvParser<Employee>(csvParserOptions, csvMapper);
+            _employeeList = csvParser
+                .ReadFromString(csvReaderOptions, csvInput)
+                .Select(x => x.Result).ToList();
+
+        }
+        // Validate Salaries
+        private bool ValidateSalaries(List<Employee> employeeList)
+        {
+            try
+            {
+                int salary = 0;
+                var isAllInts = employeeList.All(x => int.TryParse(x.Salary.ToString(), out salary));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        // Validate Non employee managers
+        private bool ValidateNonEmployeeManagers(List<Employee> employeeList)
+        {
+            try
+            {
+                if (employeeList.Where(e => string.IsNullOrEmpty(e.ManagerId) && string.IsNullOrEmpty(e.Id)).Any())
+                    return false;
+                else
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        // Validate Employee with more than one manager
+        private bool ValidateEmployees(List<Employee> employeeList)
+        {
+            try
+            {
+                if (employeeList.GroupBy(i => i.Id).Where(e => e.Count() > 1).Where(e => !string.IsNullOrEmpty(e.FirstOrDefault().ManagerId)).Any())
+                    return false;
+                else
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        // validations for single CEO
+        private  bool ValidateCEO(List<Employee> employeeList)
+        {
+            try
+            {
+                if (employeeList.GroupBy(i => i.Id).Where(e => e.Count() > 1).Where(e => string.IsNullOrEmpty(e.FirstOrDefault().ManagerId)).Any())
+                    return false;
+                else
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        // validations for single CEO
+        private bool ValidateCircularReference(List<Employee> employeeList)
+        {
+            bool circulaRefFound=false;
+            try
+            {
+                foreach (var item in employeeList)
+                {
+                    if (_employeeList.Any(r => r.Id == item.ManagerId && r.ManagerId == item.Id))
+                    {
+                        circulaRefFound = true;
+                        break;
+                    };
+                }
+                return circulaRefFound;
+            }
+            catch (Exception ex)
+            {
+                //catch errors
+                return false;
+            }
+        }
+
+        // validations for single CEO
+        public long ManagerSalaryBudget(string managerId)
+        {
+            try
+            {
+                var salaryBudget = 0;
+
+                var hgd = _employeeList.Where(f => f.ManagerId == managerId).ToList();
+                //Get employees reporting directly
+                salaryBudget += _employeeList.Where(e => e.ManagerId== managerId).Sum(s => s.Salary);
+                //Get employees reporting indirectly
+                foreach (var item in _employeeList.Where(f => f.ManagerId == managerId).ToList())
+                {
+                    var Found = _employeeList.Where(f => f.ManagerId == item.Id).Sum(s=>s.Salary);
+                    if (Found > 0) {
+
+                        salaryBudget += Found;
+
+                    }
+
+                }
+
+                return salaryBudget;
+
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        private class MappingHelper : CsvMapping<Employee>
+        {
+            public MappingHelper()
+                : base()
+            {
+                MapProperty(0, x => x.Id);
+                MapProperty(1, x => x.ManagerId);
+                MapProperty(2, x => x.Salary);
+            }
+
+        }
+
+    }
+}
